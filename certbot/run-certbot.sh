a705128f7a64ce4f976af79041cff2e982cb35ec
tei <tei952@hotmail.com>
2022-06-10 10:49:27 +0900

    modify certbot

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
+letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$MOBILESTDIO" --agree-tos --email "$EMAIL" --non-interactive --text
+letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$PAYMENTGATEWAY" --agree-tos --email "$EMAIL" --non-interactive --text
+letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$PARK" --agree-tos --email "$EMAIL" --non-interactive --text
 
-cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/"$CN"-cert1.pem
-cp /etc/letsencrypt/archive/"$CN"/chain1.pem /var/certs/chain1.pem
-cp /etc/letsencrypt/archive/"$CN"/fullchain1.pem /var/certs/fullchain1.pem
-cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/"$CN"-privkey1.pem
+cp /etc/letsencrypt/archive/"$MOBILESTDIO"/cert1.pem /var/certs/"$MOBILESTDIO"-cert1.pem
+cp /etc/letsencrypt/archive/"$MOBILESTDIO"/chain1.pem /var/certs/chain1.pem
+cp /etc/letsencrypt/archive/"$MOBILESTDIO"/fullchain1.pem /var/certs/fullchain1.pem
+cp /etc/letsencrypt/archive/"$MOBILESTDIO"/privkey1.pem /var/certs/"$MOBILESTDIO"-privkey1.pem
 
+cp /etc/letsencrypt/archive/"$PAYMENTGATEWAY"/cert1.pem /var/certs/"$PAYMENTGATEWAY"-cert1.pem
+cp /etc/letsencrypt/archive/"$PAYMENTGATEWAY"/privkey1.pem /var/certs/"$PAYMENTGATEWAY"-privkey1.pem
+
+cp /etc/letsencrypt/archive/"$PARK"/cert1.pem /var/certs/"$PARK"-cert1.pem
+cp /etc/letsencrypt/archive/"$PARK"/privkey1.pem /var/certs/"$PARK"-privkey1.pem
